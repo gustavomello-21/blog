a27feb0f4f9f322c20a611389387fd80c195b36b
gustavo melo <gustavo.melo@enjoei.com.br>
2025-01-14 07:09:18 -0300

    add create post screen

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,15 +21,15 @@ class PostsController < ApplicationController
 
   # POST /posts or /posts.json
   def create
-    @post = Post.new(post_params)
+    @post = current_user.posts.build(post_params)
 
     respond_to do |format|
       if @post.save
         format.html { redirect_to @post, notice: 'Post was successfully created.' }
         format.json { render :show, status: :created, location: @post }
       else
-        format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @post.errors, status: :unprocessable_entity }
+        format.html { render :new, status: :unprocessable_entity }
       end
     end
   end
